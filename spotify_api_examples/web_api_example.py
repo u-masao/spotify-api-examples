3917c3fcafdb670fe559c61ac0deb6ec64cbf93a
u-masao <4973920+u-masao@users.noreply.github.com>
2024-08-16 00:20:47 +0900

    関連するアーティストを検索

--- spotify_api_examples/web_api_example.py
+++ spotify_api_examples/web_api_example.py
@@ -1,4 +1,6 @@
+import click
 import spotipy
+from loguru import logger
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
 
@@ -9,7 +11,6 @@ def get_client_without_auth():
 
 
 def get_client_with_auth():
-
     sp = spotipy.Spotify(
         auth_manager=SpotifyOAuth(
             redirect_uri="http://localhost:3000/",
@@ -19,10 +20,8 @@ def get_client_with_auth():
     return sp
 
 
-def search_tracks(sp, query: str, limit: int = 20) -> None:
-    results = sp.search(q=query, limit=limit)
-    for idx, track in enumerate(results["tracks"]["items"]):
-        print(idx, track["name"])
+def search(sp, query: str, limit: int = 20, search_type="track") -> None:
+    results = sp.search(q=query, limit=limit, type=search_type)
     return results
 
 
@@ -34,15 +33,34 @@ def get_current_user_saved_tracks(sp):
     return results
 
 
-def main():
+@click.command()
+@click.argument("input_filepath", type=click.Path(exists=True))
+def main(**kwargs):
+
     sp_visitor = get_client_without_auth()
     sp = get_client_with_auth()
-    results = search_tracks(sp_visitor, "Jeff Beck", limit=20)
+
+    results = search(sp_visitor, "Jeff Beck", limit=20)
     print(results)
     results = get_current_user_saved_tracks(sp)
     print(results)
-    results = search_tracks(sp, "Jeff Beck", limit=20)
+    results = search(sp, "Jeff Beck", limit=20)
     print(results)
+
+    favorits = open(kwargs["input_filepath"], "r").read().strip().split("\n")
+    print(favorits)
+    for query in favorits:
+        results = search(sp, query, limit=1, search_type="artist")
+        artist_name = results["artists"]["items"][0]["name"]
+        artist_id = results["artists"]["items"][0]["id"]
+
+        logger.debug(results)
+        logger.info(f"{query=}")
+        logger.info(f"{artist_name=}")
+        logger.info(f"{artist_id=}")
+
+        related_artists = sp.artist_related_artists(artist_id)
+        logger.info(f"{related_artists=}")
 
 
 if __name__ == "__main__":
