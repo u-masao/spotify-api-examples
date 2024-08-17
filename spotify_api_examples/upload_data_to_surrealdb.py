5e3d428cf5e45cde566c7eb947a35765c63b5fdb
u-masao <4973920+u-masao@users.noreply.github.com>
2024-08-17 21:49:24 +0900

    Artist 情報と Relation 情報を追加

--- spotify_api_examples/upload_data_to_surrealdb.py
+++ spotify_api_examples/upload_data_to_surrealdb.py
@@ -1,117 +1,81 @@
-import json
 import asyncio
+import json
 import os
+import sys
 from pathlib import Path
 from typing import Any, List
 
 import mlflow
-import surrealdb
 import typer
 from loguru import logger
 from surrealdb import Surreal
 
 
-async def demo():
-    """Example of how to use the SurrealDB client."""
-    async with Surreal("ws://localhost:8000/rpc") as db:
+async def create_artist_relation(db, relation_from, relation_to, table="artist_relation"):
+    return await db.create(
+        table,
+        {
+            "from": relation_from,
+            "to": relation_to,
+            "id": f"{relation_from}_{relation_to}",
+        },
+    )
+
+
+async def upload_artists(artists):
+    hostname = os.environ["SURREALDB_HOST"]
+    port = os.environ["SURREALDB_PORT"]
+    async with Surreal(f"ws://{hostname}:{port}/rpc") as db:
         await db.signin(
             {"user": os.environ["SURREALDB_USER"], "pass": os.environ["SURREALDB_PASS"]}
         )
-        await db.use("test", "test")
-        await db.create(
-            "person",
-            {
-                "user": "me",
-                "pass": "safe",
-                "marketing": True,
-                "tags": ["python", "documentation"],
-            },
-        )
-        print(await db.select("person"))
-        print(
-            await db.update(
-                "person",
-                {"user": "you", "pass": "very_safe", "marketing": False, "tags": ["Awesome"]},
-            )
-        )
-        print(await db.delete("person"))
 
-        # You can also use the query method
-        # doing all of the above and more in SurrealQl
+        # set namespace and db
+        await db.use("spotify", "spotify")
 
-        # In SurrealQL you can do a direct insert
-        # and the table will be created if it doesn't exist
-        await db.query(
-            """
-        insert into person {
-            user: 'me',
-            pass: 'very_safe',
-            tags: ['python', 'documentation']
-        };
-        """
-        )
-        print(await db.query("select * from person"))
+        artist_table = "artist"
+        artist_relation_table = "artist_relation"
 
-        print(
-            await db.query(
-                """
-        update person content {
-            user: 'you',
-            pass: 'more_safe',
-            tags: ['awesome']
-        };
-        """
-            )
-        )
-        print(await db.query("delete person"))
+        # delete db
+        await db.delete(artist_table)
+        await db.delete(artist_relation_table)
 
+        for artist in artists:
+            if artist["level"] == 0:
+                data = artist["result"]["artists"]["items"][0]
+                artist_id = data["id"]
+                logger.info(f"upload: {artist_id=}")
+                await db.create(artist_table, data)
+            if artist["level"] == 1:
+                for related_artists in artist["result"]["artists"]:
+                    artist_id = related_artists["id"]
+                    logger.info(f"upload: {artist_id=}")
+                    await db.create(artist_table, related_artists)
 
-# SurrealDB への接続
-db = surrealdb.Surreal("ws://localhost:8000/rpc")
+        # result = await db.select("artist")
+        # print(json.dumps(result, indent=4, ensure_ascii=False))
+        # print(await db.query("select name from artist"))
+        # print(await db.query("select name from artist:7zsxdMsODmHKTbTB00t9wS"))
 
+        for artist in artists:
+            if artist["level"] == 1:
+                relation_from = artist["query"]
+                for related_artists in artist["result"]["artists"]:
+                    relation_to = related_artists["id"]
+                    await create_artist_relation(
+                        db, relation_from, relation_to, artist_relation_table
+                    )
 
-async def initialize_and_insert_data():
-    # データベースにサインイン
-    await db.signin({"user": os.environ["SURREALDB_USER"], "pass": os.environ["SURREALDB_PASS"]})
-    # 名前空間とデータベースを選択
-    await db.use("spotify", "artists")
-
-    # コレクションの初期化
-    await db.query("CREATE TABLE IF NOT EXISTS artist")
-
-    # アーティストデータの挿入
-    artist_data = [
-        {"artist_id": "1", "name": "Artist A", "genre": "Pop", "related_artists": ["2", "3"]},
-        {"artist_id": "2", "name": "Artist B", "genre": "Rock", "related_artists": ["1"]},
-        {"artist_id": "3", "name": "Artist C", "genre": "Pop", "related_artists": ["1"]},
-    ]
-
-    for data in artist_data:
-        await db.create("artist", data)
-
-
-async def fetch_related_artists(artist_id):
-    # 関連アーティストの取得
-    result = await db.query(
-        "SELECT * FROM artist WHERE artist_id IN $related_artists", {"related_artists": artist_id}
-    )
-    return result
-
-
-# 非同期処理の実行
-async def access_db():
-    await initialize_and_insert_data()
-
-    related_artists = await fetch_related_artists("1")
-    print(related_artists)
-
-    await db.close()
+        # result = await db.select("artist_relation")
+        # print(json.dumps(result, indent=4, ensure_ascii=False))
+        # search_id = "artist_relation:7xIfi0ePXzLGlYO5lFjnvu_58YNdEUQRt7LNccL1icwYL"
+        # print(await db.query(f"select * from {search_id}"))
 
 
 def upload_data_to_surrealdb(
     artists_info: List[Any],
 ):
-    access_db()
+    asyncio.run(upload_artists(artists_info))
 
 
 def load_input_data(input_filepath: str):
@@ -129,7 +93,6 @@ def main(
     output_filepath: str = typer.Argument(..., help="出力ファイルへのパス"),
 ):
 
-
     # init log
     logger.info("Processing dataset...")
     mlflow.set_experiment("upload_data")
@@ -144,11 +107,9 @@ def main(
 
     # load input data
     artists_info = load_input_data(input_filepath)
-    logger.info(f"{artists_info=}")
 
     # 処理
     results = upload_data_to_surrealdb(artists_info)
-    asyncio.run(demo())
 
     # ファイル出力
     Path(output_filepath).parent.mkdir(parents=True, exist_ok=True)
@@ -159,4 +120,5 @@ def main(
 
 
 if __name__ == "__main__":
+    logger.configure(handlers=[{"sink": sys.stderr, "backtrace": False, "diagnose": False}])
     app()
